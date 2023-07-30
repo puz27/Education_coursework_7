4f7ad7fbadc77d54eea0282ecf8a822e9ba563aa
Nikolay <n.avramenko87@gmail.com>
2023-07-30 12:47:53 +0300

    add celery

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,11 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_TIMEZONE = "Europe/Moscow"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
