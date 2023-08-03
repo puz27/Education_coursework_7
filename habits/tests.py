4941df7a10ff96c48b17ec8c32e6ef3095c429cd
Nick_Linux <n.avramenko87@gmail.com>
2023-08-04 00:08:57 +0300

    test not work

--- habits/tests.py
+++ habits/tests.py
@@ -1,5 +1,4 @@
 from rest_framework.test import APITestCase
-from habits.models import Habit
 from rest_framework import status
 from users.models import Users
 
@@ -8,7 +7,13 @@ class HabitTestCase(APITestCase):
 
     def setUp(self):
         """ Prepare testing """
-        self.user = Users(email="test@gmail.com")
+        self.user = Users(
+            email="test@gmail.com",
+            is_superuser=False,
+            is_staff=False,
+            is_active=True,
+            )
+
         self.user.set_password("test")
         self.user.save()
 
@@ -20,75 +25,70 @@ class HabitTestCase(APITestCase):
         self.access_token = response.json().get("access")
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
 
-        self.course = Users.objects.create(
-            title="Test Course",
-            description="Test Course description",
-        )
 
-        self.lesson = Users.objects.create(
-            title="Test Lesson",
-            description="Test Lesson description",
-            course=self.course
-        )
+    def test_create_habit(self):
 
-    def test_create_subscription(self):
-        data = {'id': 1,
-                'is_subscribed': False,
-                "owner": 1,
-                "course": 1
-                }
+        data = {
+            "place": "At home.",
+            "time": "14:00:00",
+            "action": "Clean windows.",
+            "is_pleasant": "False",
+            "frequency": "SUNDAY",
+            "award": "To drink bear.",
+            "duration": 10,
+            "is_public": "True",
+            "owner": 1
+            }
 
-        response = self.client.post("/api/v1/subscription/create/", data)
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_201_CREATED
-        )
-        self.assertTrue(Subscription.objects.all().exists())
+        response = self.client.post("api/v1/habit/create/", data)
 
-    def test_get_subscriptions(self):
-        response = self.client.get("/api/v1/subscription/")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # self.assertTrue(Habit.objects.all().exists())
 
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
-
-    def test_get_lessons(self):
-            response = self.client.get("/api/v1/lesson/")
-
-            self.assertEqual(
-                response.status_code,
-                status.HTTP_200_OK
-            )
-
-    def test_validate_lesson(self):
-        data = {"title": "Test Lesson23",
-                "description": "https://vk.com"
-                }
-
-        response = self.client.post("/api/v1/lesson/create/", data)
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_400_BAD_REQUEST
-        )
-
-    def test_get_courses(self):
-            response = self.client.get("/api/v1/course/")
-
-            self.assertEqual(
-                response.status_code,
-                status.HTTP_200_OK
-            )
-
-    def test_validate_course(self):
-            data = {"title": "Test Lesson23",
-                    "description": "https://vk.com"
-                    }
-
-            response = self.client.post("/api/v1/course/create/", data)
-
-            self.assertEqual(
-                response.status_code,
-                status.HTTP_400_BAD_REQUEST
-            )
+    # def test_get_subscriptions(self):
+    #     response = self.client.get("/api/v1/subscription/")
+    #
+    #     self.assertEqual(
+    #         response.status_code,
+    #         status.HTTP_200_OK
+    #     )
+    #
+    # def test_get_lessons(self):
+    #         response = self.client.get("/api/v1/lesson/")
+    #
+    #         self.assertEqual(
+    #             response.status_code,
+    #             status.HTTP_200_OK
+    #         )
+    #
+    # def test_validate_lesson(self):
+    #     data = {"title": "Test Lesson23",
+    #             "description": "https://vk.com"
+    #             }
+    #
+    #     response = self.client.post("/api/v1/lesson/create/", data)
+    #
+    #     self.assertEqual(
+    #         response.status_code,
+    #         status.HTTP_400_BAD_REQUEST
+    #     )
+    #
+    # def test_get_courses(self):
+    #         response = self.client.get("/api/v1/course/")
+    #
+    #         self.assertEqual(
+    #             response.status_code,
+    #             status.HTTP_200_OK
+    #         )
+    #
+    # def test_validate_course(self):
+    #         data = {"title": "Test Lesson23",
+    #                 "description": "https://vk.com"
+    #                 }
+    #
+    #         response = self.client.post("/api/v1/course/create/", data)
+    #
+    #         self.assertEqual(
+    #             response.status_code,
+    #             status.HTTP_400_BAD_REQUEST
+    #         )
