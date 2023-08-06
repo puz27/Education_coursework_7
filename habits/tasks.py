3821911b02f60892b91863a7e920df84e655ad86
Nick_Linux <n.avramenko87@gmail.com>
2023-08-06 22:45:20 +0300

    add shared task

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,6 +1,23 @@
 from celery import shared_task
+from habits.models import Habit
+from datetime import datetime
 
 
 @shared_task(name="check_habit_time")
 def check_habit_time():
-    print("Check last login user.")
+    today = datetime.today().weekday()
+    current_time = datetime.now().time()
+    habits = Habit.objects.filter()
+    for habit in habits:
+        print(habit)
+        print(habit.frequency)
+        if habit.frequency == "DAILY":
+            if habit.time == current_time:
+                print("SEND MESSAGE TO TELEGRAM")
+        else:
+            print(f"DAY WEEK:{today}")
+            if habit.frequency == today:
+                if habit.time == current_time:
+                    print("SEND MESSAGE TO TELEGRAM")
+                    print("SEND MESSAGE TO TELEGRAM")
+
